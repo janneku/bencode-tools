45ebced10f5c1e9121006136647b0f9b0f73674c
Janne Kulmala <janne.t.kulmala@tut.fi>
2010-06-28 16:33:46 +0300

    Specify data types with standard Python names instead of INT, INT_KEY, STRING, STRING_KEY, etc [PERFECTIVE]
    Now one specifies a validator with {str: int} instead of {STRING_KEY: INT}

--- typevalidator.py
+++ typevalidator.py
@@ -5,30 +5,29 @@
 #
 # Originally written by Heikki Orsila <heikki.orsila@iki.fi> on 2009-09-12
 
-ANY = 'a'
-BOOL = True
-INT = 0
-STRING = 'x'
-FLOAT = 1.0
-ZERO_OR_MORE = '*'
-ONE_OR_MORE = '+'
+from types import FunctionType
 
-class BOOL_KEY:
-    key = None
-class INT_KEY:
-    key = None
-class FLOAT_KEY:
-    key = None
+# BOOL*, INT*, STRING* and FLOAT* are used for backward compability
+# with the old interface. New code should use bool/int/str/float directly.
+BOOL = bool
+BOOL_KEY = bool
+INT = int
+INT_KEY = int
+STRING = str
+STRING_KEY = str
+FLOAT = float
+FLOAT_KEY = float
+
+class ANY:
+    pass
+class ZERO_OR_MORE:
+    pass
+class ONE_OR_MORE:
+    pass
+
 class OPTIONAL_KEY:
     def __init__(self, key):
         self.key = key
-class STRING_KEY:
-    key = None
-keytypemap = {BOOL_KEY: type(BOOL),
-              FLOAT_KEY: type(FLOAT),
-              INT_KEY: type(INT),
-              STRING_KEY: type(STRING),
-             }
 
 class Invalid_Format_Object(Exception):
     def __init__(self, reason=''):
@@ -38,6 +37,8 @@ class Invalid_Format_Object(Exception):
         return self.reason
 
 def validate_list(fmt, o):
+    if type(o) != list:
+        return False
     fmt = list(fmt)
     o = list(o)
     while len(fmt) > 0:
@@ -65,16 +66,17 @@ def validate_list(fmt, o):
     return len(o) == 0
 
 def validate_dict(fmt, o):
+    if type(o) != dict:
+        return False
     for key in fmt.keys():
         if o.has_key(key) and validate(fmt[key], o[key]):
             continue
-        t = keytypemap.get(key)
-        if t != None:
-            # STRING_KEY, INT_KEY, ...
+        if type(key) == type:
+            # str, int, ...
             for okey in o.keys():
-                if type(okey) != t:
+                if type(okey) != key:
                     # Associate int with long
-                    if t != int or type(okey) != long:
+                    if key != int or type(okey) != long:
                         return False
                 if validate(fmt[key], o[okey]) == False:
                     return False
@@ -91,65 +93,87 @@ def validate(fmt, o):
         return True
 
     # Is this a user defined checker function?
-    if callable(fmt):
+    if type(fmt) == FunctionType:
         return fmt(o)
-
-    if type(fmt) != type(o):
-        # Associate int type with long. We don't use LONG as a validator
-        # keyword, just INT
-        if type(fmt) != int or type(o) != long:
-            return False
-
-    if type(fmt) == list:
+    elif type(fmt) == list:
         return validate_list(fmt, o)
     elif type(fmt) == dict:
         return validate_dict(fmt, o)
+    elif type(fmt) == type:
+        if fmt != type(o):
+            # Associate int type with long. We don't use LONG as a validator
+            # keyword, just int
+            if fmt != int or type(o) != long:
+                return False
+    # If given format is a not a type but a value, compare input to the given value
+    elif fmt != o:
+        return False
+
     return True
 
 def test_validate():
-    assert(validate([STRING, [ONE_OR_MORE, INT], [ZERO_OR_MORE, INT], {'a': INT, 1: STRING}], ['fff', [0], [], {'a': 0, 1: 'foo'}]))
-    assert(validate([STRING, [ONE_OR_MORE, INT], [ZERO_OR_MORE, INT], {'a': INT, 1: STRING}], [1, [0], [], {'a': 0, 1: 'foo'}]) == False)
-    assert(validate([STRING, [ONE_OR_MORE, INT], [ZERO_OR_MORE, INT], {'a': INT, 1: STRING}], ['fff', [], [], {'a': 0, 1: 'foo'}]) == False)
-    assert(validate([ONE_OR_MORE, INT, ZERO_OR_MORE, STRING], [1, 1, 1]))
-    assert(validate([ONE_OR_MORE, INT, ZERO_OR_MORE, STRING], [1, 1, 1, 's']))
-    assert(validate([ZERO_OR_MORE, INT, ONE_OR_MORE, STRING], [1, 1, 1, 's']))
-    assert(validate([ZERO_OR_MORE, INT, ONE_OR_MORE, STRING], [1, 1, 1]) == False)
-    assert(validate([ZERO_OR_MORE, INT, ONE_OR_MORE, STRING], ['d']))
-    assert(validate([ZERO_OR_MORE, INT, ONE_OR_MORE, STRING], []) == False)
+    assert(validate([str, [ONE_OR_MORE, int], [ZERO_OR_MORE, int], {'a': int, 1: str}], ['fff', [0], [], {'a': 0, 1: 'foo'}]))
+    assert(validate([str, [ONE_OR_MORE, int], [ZERO_OR_MORE, int], {'a': int, 1: str}], [1, [0], [], {'a': 0, 1: 'foo'}]) == False)
+    assert(validate([str, [ONE_OR_MORE, int], [ZERO_OR_MORE, int], {'a': int, 1: str}], ['fff', [], [], {'a': 0, 1: 'foo'}]) == False)
+    assert(validate([ONE_OR_MORE, int, ZERO_OR_MORE, str], [1, 1, 1]))
+    assert(validate([ONE_OR_MORE, int, ZERO_OR_MORE, str], [1, 1, 1, 's']))
+    assert(validate([ZERO_OR_MORE, int, ONE_OR_MORE, str], [1, 1, 1, 's']))
+    assert(validate([ZERO_OR_MORE, int, ONE_OR_MORE, str], [1, 1, 1]) == False)
+    assert(validate([ZERO_OR_MORE, int, ONE_OR_MORE, str], ['d']))
+    assert(validate([ZERO_OR_MORE, int, ONE_OR_MORE, str], []) == False)
 
     assert(validate(lambda x: x % 2 == 0, 0))
     assert(validate(lambda x: x % 2 == 0, 1) == False)
 
-    assert(validate({STRING_KEY: STRING}, {'a': 'b'}))
-    assert(validate({STRING_KEY: STRING}, {1: 'b'}) == False)
-    assert(validate({STRING_KEY: STRING}, {'a': 1}) == False)
-    assert(validate({STRING_KEY: INT}, {'a': 1}))
-    assert(validate({INT_KEY: STRING}, {1: 'a'}))
-    assert(validate({INT_KEY: STRING}, {1: 'a', 'b': 2}) == False)
+    assert(validate({str: str}, {'a': 'b'}))
+    assert(validate({str: str}, {1: 'b'}) == False)
+    assert(validate({str: str}, {'a': 1}) == False)
+    assert(validate({str: int}, {'a': 1}))
+    assert(validate({int: str}, {1: 'a'}))
+    assert(validate({int: str}, {1: 'a', 'b': 2}) == False)
 
     # Extra keys in dictionary are allowed
-    assert(validate({'x': INT}, {'x': 1, 'y': 1}))
+    assert(validate({'x': int}, {'x': 1, 'y': 1}))
     # Missing key fails
-    assert(validate({'x': INT}, {'y': 1}) == False)
+    assert(validate({'x': int}, {'y': 1}) == False)
 
     # OK
-    assert(validate({'x': INT, STRING_KEY: INT}, {'x': 1, 'y': 1}))
+    assert(validate({'x': int, str: int}, {'x': 1, 'y': 1}))
     # Non-string key
-    assert(validate({'x': INT, STRING_KEY: INT}, {'x': 1, 1: 1}) == False)
+    assert(validate({'x': int, str: int}, {'x': 1, 1: 1}) == False)
     # Missing key, but correct key type
-    assert(validate({'x': INT, STRING_KEY: INT}, {'y': 1}) == False)
+    assert(validate({'x': int, str: int}, {'y': 1}) == False)
 
-    assert(validate({'x': BOOL}, {'x': False}))
-    assert(validate({'x': BOOL}, {'x': 0}) == False)
+    assert(validate({'x': bool}, {'x': False}))
+    assert(validate({'x': bool}, {'x': 0}) == False)
 
     # Test OPTIONAL_KEY
-    assert(validate({OPTIONAL_KEY('x'): INT}, {}))
-    assert(validate({OPTIONAL_KEY('x'): INT}, {'x': 1}))
-    assert(validate({OPTIONAL_KEY('x'): INT}, {'x': 'invalid'}) == False)
+    assert(validate({OPTIONAL_KEY('x'): int}, {}))
+    assert(validate({OPTIONAL_KEY('x'): int}, {'x': 1}))
+    assert(validate({OPTIONAL_KEY('x'): int}, {'x': 'invalid'}) == False)
 
     # Test that int and long are equivalent
-    assert(validate({'x': INT}, {'x': 0L}))
-    assert(validate({INT_KEY: ANY}, {0L: 'x'}))
+    assert(validate({'x': int}, {'x': 0L}))
+    assert(validate({int: ANY}, {0L: 'x'}))
+
+    # Typevalidator can be used to check that values are equal
+    assert(validate([1, 2, 3, [True, 'a']], [1, 2, 3, [True, 'a']]))
+    assert(validate('foo', 'bar') == False)
+
+def benchmark():
+    specification = {'uid': str,
+                     'ids': [ZERO_OR_MORE, int],
+                     'purposes': [ZERO_OR_MORE, str],
+                     'metas': [ZERO_OR_MORE, {}],
+                    }
+    request = {'uid': '0123456789abcdef',
+               'ids': [0, 1, 2, 3, 4],
+               'purposes': ['a', 'b', 'c', 'd', 'e'],
+               'metas': [{}, {}, {}, {}, {}],
+              }
+    for i in xrange(100000):
+        if not validate(specification, request):
+            assert(False)
 
 if __name__ == '__main__':
     test_validate()
