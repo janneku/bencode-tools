1b262e39105434dc13829164edd38e8bf11535b2
Heikki Orsila <heikki.orsila@iki.fi>
2011-03-07 00:33:36 +0200

    libtypevalidator: Implement bencode strings [PERFECTIVE]

--- bencodetest.c
+++ bencodetest.c
@@ -24,6 +24,25 @@ static void inttest(const char *s, size_t len, long long expected, int success)
 	ben_free(b);
 }
 
+static void strtest(const char *s, size_t len, const char *expected, int success)
+{
+	struct bencode *b;
+	b = ben_decode(s, len);
+	if (success && b == NULL) {
+		fprintf(stderr, "%s/%zd should have succeeded\n", s, len);
+		exit(1);
+	}
+	if (success && strcmp(b->s.s, expected) != 0) {
+		fprintf(stderr, "%s/%zd should have value %s\n", s, len, expected);
+		exit(1);
+	}
+	if (!success && b != NULL) {
+		fprintf(stderr, "%s/%zd should have failed\n", s, len);
+		exit(1);
+	}
+	ben_free(b);
+}
+
 int main(void)
 {
 	inttest("i-1e", 4, -1, 1);
@@ -32,5 +51,10 @@ int main(void)
 	inttest("i1e", 3, 1, 1);
 	inttest("ie", 2, 0, 0);
 	inttest("i1ke", 4, 1, 0);
+
+	strtest("0:", 2, "", 1);
+	strtest("7:marklar", 9, "marklar", 1);
+	strtest("7:marklar", 8, "marklar", 0);
+
 	return 0;
 }
--- libtypevalidator.c
+++ libtypevalidator.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
+#include <errno.h>
 
 static size_t find(const char *data, size_t len, size_t off, char c)
 {
@@ -43,6 +44,7 @@ static struct bencode *decode_int(const char *data, size_t len, size_t *off)
 	struct bencode *b;
 	char *endptr;
 	size_t pos;
+	long long ll;
 
 	pos = find(data, len, *off + 1, 'e');
 	if (pos == -1)
@@ -51,21 +53,86 @@ static struct bencode *decode_int(const char *data, size_t len, size_t *off)
 	if (slen == 0 || slen >= sizeof buf)
 		return invalid("bad int slen", *off);
 	assert(slen < sizeof buf);
-	memcpy(buf, &data[*off + 1], slen);
+	memcpy(buf, data + *off + 1, slen);
 	buf[slen] = 0;
-	b = alloc(BENCODE_INT);
-	b->ll = strtoll(buf, &endptr, 10);
-	if (*endptr != 0) {
-		free(b);
+	errno = 0;
+	ll = strtoll(buf, &endptr, 10);
+	if (errno == ERANGE || *endptr != 0)
 		return invalid("bad int string", *off);
+	b = alloc(BENCODE_INT);
+	if (b == NULL) {
+		fprintf(stderr, "bencode: No memory for int\n");
+		return NULL;
 	}
+	b->ll = ll;
 	*off = pos;
 	return b;
 }
 
+static size_t read_size_t(const char *buf)
+{
+	char *endptr;
+	long long ll;
+	size_t s;
+
+	errno = 0;
+	/* Note: value equal to ((size_t) -1) is not valid */
+	ll = strtoll(buf, &endptr, 10);
+	if (errno == ERANGE || *endptr != 0)
+		return -1;
+	if (ll < 0)
+		return -1;
+	s = (size_t) ll;
+	if (ll != (long long) s)
+		return -1;
+	return s;
+}
+
 static struct bencode *decode_str(const char *data, size_t len, size_t *off)
 {
-	assert(0);
+	char buf[21];
+	size_t pos;
+	size_t slen;
+	size_t datalen;
+	struct bencode *b;
+	size_t newoff;
+
+	pos = find(data, len, *off + 1, ':');
+	if (pos == -1)
+		return overflow(*off);
+	slen = pos - *off;
+	if (slen == 0 || slen >= sizeof buf)
+		return invalid("no string length", *off);
+	assert(slen < sizeof buf);
+	memcpy(buf, data + *off, slen);
+	buf[slen] = 0;
+
+	/* Read the string length */
+	datalen = read_size_t(buf);
+	if (datalen == -1)
+		return invalid("invalid string length", *off);
+
+	newoff = pos + 1 + datalen;
+	if (newoff > len)
+		return invalid("too long a string (data out of bounds)", *off);
+
+	/* Allocate string structure and copy data into it */
+	b = alloc(BENCODE_STR);
+	if (b == NULL) {
+		fprintf(stderr, "bencode: No memory for str structure\n");
+		return NULL;
+	}	
+	b->s.s = malloc(datalen);
+	if (b->s.s == NULL) {
+		free(b);
+		fprintf(stderr, "bencode: No memory for string\n");
+		return NULL;
+	}
+	memcpy(b->s.s, data + pos + 1, datalen);
+	b->s.len = datalen;
+
+	*off = newoff;
+	return b;
 }
 
 static struct bencode *decode(const char *data, size_t len, size_t *off, int l)
@@ -110,7 +177,18 @@ void ben_free(struct bencode *b)
 {
 	if (b == NULL)
 		return;
-	assert(b->type == BENCODE_INT);
+	switch (b->type) {
+	case BENCODE_INT:
+		break;
+	case BENCODE_STR:
+		free(b->s.s);
+		b->s.s = NULL;
+		b->s.len = 0;
+		break;
+	default:
+		fprintf(stderr, "bencode: invalid type: %d\n", b->type);
+		exit(1);
+	}
 	b->type = 0;
 	free(b);
 }
